c5e776a64c7458f4ece2a89974c11fc0a5a5df7e
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-13 18:02:55 +0200

    creata lista email

---
+++ mail/js/script.js
@@ -0,0 +1,3 @@
+const listaEmail = ['barbone1912@gmail.com', 'ciao@gmail.com', 'rossellarusso@gmail.com', 'luciadidio@gmail.com','barbone1912@gmail.com', 'ciao@gmail.com', 'rossellarusso@gmail.com', 'luciadidio@gmail.com'];
+
+console.log(listaEmail)
--- mail/readme.md
+++ mail/readme.md
@@ -1,3 +1,6 @@
 Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
+
+- creare una lista con tutte le email autorizzate ad accedere
+- 
