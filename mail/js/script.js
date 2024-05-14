5ef60768652465a9f8a8db5d2d54a8580771587f
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-14 09:55:06 +0200

    esercizio email

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,3 +1,11 @@
 const listaEmail = ['barbone1912@gmail.com', 'ciao@gmail.com', 'rossellarusso@gmail.com', 'luciadidio@gmail.com','barbone1912@gmail.com', 'ciao@gmail.com', 'rossellarusso@gmail.com', 'luciadidio@gmail.com'];
 
 console.log(listaEmail)
+
+const email = prompt('inserisci la tua email')
+
+if (listaEmail.includes(email)){
+    console.log ('puoi accedere');
+}else{
+    console.log('non puoi entrare')
+}
